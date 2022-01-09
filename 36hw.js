f33572854279e6ccd6ac5c416903e25a56ab9ab4
Aidana777 <aidananiazalieva27@gmail.com>
2022-01-09 19:02:21 +0600

    first commit

---
+++ 36hw.js
@@ -0,0 +1,5 @@
+document.querySelector("header").classList.add("header");
+document.querySelector("header>div").classList.add("container");
+document.querySelector("header menu").classList.add("header__menu");
+document.querySelector("header div div").classList.add("header__content");
+document.querySelector("header button").classList.add("header__menu-button")
